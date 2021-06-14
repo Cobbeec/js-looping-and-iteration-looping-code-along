cb7142318b3396ea7f85f9c48fba51d5ff86a29c
Cobbeec <cobbeec@gmail.com>
2021-06-14 17:44:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(names) {
+    let greeting = [];
+  for (let i = 0; i < names.length; i++) {
+   greeting.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return greeting;
+}
+
+function countDown(n) {
+    while(n >=0){
+        console.log(n)
+        n-- 
+    }
+}
